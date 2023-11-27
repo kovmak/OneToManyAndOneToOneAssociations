6fee3b4f1cb054b929a8ede8ed6800df5e9add9c
krnelx <129564731+kovmak@users.noreply.github.com>
2023-11-27 19:50:58 +0200

    Add files via upload

---
+++ practic_4_oop/src/Account.java
@@ -0,0 +1,55 @@
+public class Account {
+
+    private String accountNumber;
+    private double balance;
+    private Person owner;
+
+
+    public Account(String accountNumber,double balance, Person owner){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public class BankCard{
+        private String cardNumber;
+        private Account account;
+
+        public BankCard(String cardNumber){
+            this.cardNumber = cardNumber;
+        }
+
+        public void withdraw(double amount) {
+            if (amount > 0 && amount <= balance) {
+                balance -= amount;
+            }
+            else {
+                System.out.println("Withdrawal failed. Insufficient funds.");
+            }
+        }
+
+        public void setBankAccount(Account account) {
+            this.account = account;
+        }
+
+        public String getCardNumber() {
+            return cardNumber;
+        }
+
+        public Account getBankAccount() {
+            return account;
+        }
+    }
+
+    public void addCard(BankCard card){
+        card.setBankAccount(this);
+    }
+}
---
+++ practic_4_oop/src/Person.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+    private String name;
+    private Account account;
+    private List<Account.BankCard> bankCards;
+
+    public Person(String name) {
+        this.name = name;
+        this.bankCards = new ArrayList<>();
+    }
+
+    public void authorize() {
+        System.out.println(name + " is authorized.");
+    }
+
+    public void setBankAccount(Account account) {
+        this.account = account;
+    }
+
+    public void addBankCard(Account.BankCard bankCard) {
+        bankCards.add(bankCard);
+    }
+
+    public Account getBankAccount() {
+        return account;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void showInformation(String selectedCurrency) {
+        System.out.println("Person: " + name);
+        if (account != null) {
+            System.out.println("Bank Account: " + account.getAccountNumber());
+            System.out.println("Balance: " + account.getBalance() + " " + selectedCurrency);
+            System.out.println("Bank Cards:");
+            for (Account.BankCard card : bankCards) {
+                System.out.println("- " + card.getCardNumber());
+            }
+        } else {
+            System.out.println("No bank account associated.");
+        }
+    }
+
+    public Account.BankCard getBankCard(String cardNumber) {
+        for (Account.BankCard card : bankCards) {
+            if (card.getCardNumber().equals(cardNumber)) {
+                return card;
+            }
+        }
+        return null;
+    }
+}
